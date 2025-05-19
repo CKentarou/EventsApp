0a788aa69c84670c5d8cd8d31596a8e2a2f3209a
Cliff Kentaro Shimada <k023c0018@g.neec.ac.jp>
2025-05-19 13:16:29 +0900

    Revert "omniauthの設定"
    This reverts commit 0015646aa1a2a0d954e9e307e4c6557991a46577.

--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.development? || Rails.env.test?
-    provider :github, "Ov23liYidnkSB9thXfjy", "0ab12f338048bfb06deb8f02fa928b9ed2d55f95"
-  else
-    provider :github,
-      Rails.application.credentials.github[:client_id],
-      Rails.application.credentials.github[:client_secret]
-  end
-end
