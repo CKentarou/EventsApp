492ed3d85027b678cb76d0b7e8b8be1de7b7d523
CKentarou <k023c0018@g.neec.ac.jp>
2025-05-20 20:41:10 +0900

    イベント参加処理実装

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,16 @@
 class TicketsController < ApplicationController
+  def new
+    raise ActionController::RoutingErro, "ログイン状態でTicketsController#newにアクセス"
+  end
+
+  def create
+    event = Event.find(params[:event_id])
+    @ticket = current_user.tickets.build do |t|
+      t.event = event
+      t.comment = params[:ticket][:comment]
+    end
+    if @ticket.save
+      redirect_to event, notice: 'チケットを取得しました'
+    end
+  end
 end
