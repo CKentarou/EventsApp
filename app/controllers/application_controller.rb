8ef6bdc623ca853121382c3d2e044c1b4236a52f
CKentarou <k023c0018@g.neec.ac.jp>
2025-05-20 15:57:10 +0900

    イベント一覧機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+    return if User.count.zero?
+    @current_user ||= User.find_by(id: session[:user_id])
   end
-
   def authenticate
     return if logged_in?
     redirect_to root_path, alert: "ログインしてください"
