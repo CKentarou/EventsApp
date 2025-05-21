730f3f9cc74385884b5b270c34b255d0585fb569
CKentarou <k023c0018@g.neec.ac.jp>
2025-05-21 09:35:46 +0900

    イベント参加取り消し機能実装

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -13,4 +13,10 @@ class TicketsController < ApplicationController
       redirect_to event, notice: 'チケットを取得しました'
     end
   end
+
+  def destroy
+    ticket = current_user.tickets.find_by!(event_id: params[:event_id])
+    ticket.destroy!
+    redirect_to event_path(params[:event_id]), notice: 'このイベントへの参加をキャンセルしました'
+  end
 end
