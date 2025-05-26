cb0f4fcbf0dbb70709069806ff64dd1964895e0c
CKentarou <k023c0018@g.neec.ac.jp>
2025-05-26 10:21:37 +0900

    イベント作成機能のテストを追加し、ユーザーのサインインをセットアップ

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,4 +3,8 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
   include SignInHelper
+
+  teardown do
+    Capybara.reset_sessions!
+  end
 end
--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -1,6 +1,10 @@
 require "application_system_test_case"
 
 class EventsTest < ApplicationSystemTestCase
+  setup do
+    sign_in_as(FactoryBot.create(:user))
+  end
+
   test "/event/:idページを表示する" do
     event = FactoryBot.create(:event)
     visit event_url(event)
@@ -9,9 +13,36 @@ class EventsTest < ApplicationSystemTestCase
   end
 
   test "/event/new/ページを表示する" do
-    sign_in_as(FactoryBot.create(:user))
-
     visit new_event_url
     assert_selector "h2", text: "イベント作成"
+  end
+
+  test "/event/new/ページでフォームを記入して登録する" do  
+    visit new_event_url
+    assert_selector "h2", text: "イベント作成"
+  
+    fill_in "イベント名", with: "テストイベント"
+    fill_in "場所", with: "テスト会場"
+    fill_in "イベント内容", with: "テストイベントの内容"
+  
+    start_at = Time.current
+    end_at = start_at + 1.hour
+  
+    start_at_field = "event_start_at"
+    select start_at.strftime("%Y"), from: "#{start_at_field}_1i"
+    select I18n.l(start_at, format: '%B'), from: "#{start_at_field}_2i" 
+    select start_at.strftime("%-d"), from: "#{start_at_field}_3i"
+    select start_at.strftime("%H"), from: "#{start_at_field}_4i"
+    select start_at.strftime("%M"), from: "#{start_at_field}_5i"
+  
+    end_at_field = "event_end_at"
+    select end_at.strftime("%Y"), from: "#{end_at_field}_1i"
+    select I18n.l(end_at, format: '%B'), from: "#{end_at_field}_2i" 
+    select end_at.strftime("%-d"), from: "#{end_at_field}_3i"
+    select end_at.strftime("%H"), from: "#{end_at_field}_4i"
+    select end_at.strftime("%M"), from: "#{end_at_field}_5i"
+  
+    click_on "イベントを作成"
+    assert_selector "div.alert", text: "作成しました"
   end
 end
