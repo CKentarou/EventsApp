6fbfd10c48292391202d11a3bba82b8a40750b20
CKentarou <k023c0018@g.neec.ac.jp>
2025-05-26 09:26:07 +0900

    新しいイベント作成ページのテストを追加

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -7,4 +7,11 @@ class EventsTest < ApplicationSystemTestCase
   
     assert_selector "h1", text: event.name
   end
+
+  test "/event/new/ページを表示する" do
+    sign_in_as(FactoryBot.create(:user))
+
+    visit new_event_url
+    assert_selector "h2", text: "イベント作成"
+  end
 end
