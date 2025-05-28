a827984accca6eba1f90f745ef2e3417d4fb01dd
CKentarou <k023c0018@g.neec.ac.jp>
2025-05-28 19:46:51 +0900

    イベントモデルに画像の添付および削除機能を追加し、イベント作成・編集フォームに画像アップロード機能を実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  attr_accessor :remove_image
+  before_save :remove_image_if_user_accept
   belongs_to :owner, class_name: 'User'
   has_many :tickets, dependent: :destroy
   has_one_attached :image
@@ -16,6 +18,10 @@ class Event < ApplicationRecord
   end
 
   private
+
+  def remove_image_if_user_accept
+    self.image = nil if ActiveRecord::Type::Boolean.new.cast(remove_image)
+  end
 
   def start_at_should_be_before_end_at
     return unless start_at && end_at
